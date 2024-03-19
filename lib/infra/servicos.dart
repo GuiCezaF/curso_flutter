11791e79fc5d71da789501572e82f652a921e0fa
Guilherme Felipe <guilherme.felipe@retta.com.br>
2024-03-19 15:11:49 -0300

    [Curso Flutter] - adicionando o iniciar serviços e atualizando o registrar dependencia (tanto api quanto in memory)

--- lib/infra/servicos.dart
+++ lib/infra/servicos.dart
@@ -1,21 +1,31 @@
+import 'package:curso_flutter/domain/cliente/repo_cliente.dart';
 import 'package:curso_flutter/domain/login/repo_login.dart';
 import 'package:curso_flutter/infra/dependencia.dart';
 
-Future iniciarRepositorio() async {
-  void registrarDependenciasInMemory() {
-    Dependencia.adicionarOuSubstituir<IloginRepo>(LoginInMemory());
-  }
+enum TipoRepositorio {
+  inMemory,
+  API;
+}
 
-  void registrarDependenciasApi() {
-    Dependencia.adicionarOuSubstituir<IloginRepo>(LoginRepoAPI());
-  }
-  
-  bool inModoMemory = true;
+Future iniciarServicos() async {
+  TipoRepositorio tipoRepositorio = TipoRepositorio.inMemory;
 
-  if (inModoMemory) {
-    registrarDependenciasInMemory();
-  } else {
-    registrarDependenciasApi();
+  switch (tipoRepositorio) {
+    case TipoRepositorio.inMemory:
+      registrarDependeciasInMemory();
+      break;
+    case TipoRepositorio.API:
+      registrarDependeciasApi();
+      break;
   }
+}
 
+void registrarDependeciasApi() {
+  Dependencia.adicionarOuSubstituir<IloginRepo>(LoginRepoAPI());
+  Dependencia.adicionarOuSubstituir<IClienteRepo>(ClienteRepoAPI());
+}
+
+void registrarDependeciasInMemory() {
+  Dependencia.adicionarOuSubstituir<IloginRepo>(LoginRepoInMemory());
+  Dependencia.adicionarOuSubstituir<IClienteRepo>(ClienteRepoInMemory());
 }
