4ea9fb2c2d3b78eecf15ff60ec892a6a5d859b3f
Guilherme Felipe <guilherme.felipe@retta.com.br>
2024-03-20 11:26:23 -0300

    [Curso Flutter] - implementando os métodos de listagem dos clientes.

--- lib/domain/cliente/cliente.dart
+++ lib/domain/cliente/cliente.dart
@@ -1,7 +1,24 @@
-class Cliente {
-  late String nomeRazao;
-  late String nomeFantasia;
-  late String cpfCnpj;
+import 'dart:convert';
 
-  Cliente(String nomeRazao, String nomeFantasia, String cpfCnpj) {}
+class Cliente {
+  String nomeRazao;
+  String nomeFantasia;
+  String cpfCnpj;
+
+  Cliente.fromJsonObject(Map<String, dynamic> jsonObject)
+      : nomeRazao = jsonObject['nomeRazao'],
+        nomeFantasia = jsonObject['nomeFantasia'],
+        cpfCnpj = jsonObject['cpfCnpj'];
+
+  static List<Cliente> fromJson(String jsonStr) {
+    List<Cliente> lista = [];
+
+    var jsonArray = jsonDecode(jsonStr);
+
+    for (Map<String, dynamic> jsonObject in jsonArray) {
+      lista.add(Cliente.fromJsonObject(jsonObject));
+    }
+
+    return lista;
+  }
 }
--- lib/domain/cliente/repo_cliente.dart
+++ lib/domain/cliente/repo_cliente.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:curso_flutter/domain/cliente/cliente.dart';
+import 'package:curso_flutter/infra/request_helper.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
 
 abstract class IClienteRepo {
   Future<List<Cliente>> listar(String filtroPesquisa);
@@ -6,16 +11,28 @@ abstract class IClienteRepo {
 
 class ClienteRepoAPI implements IClienteRepo {
   @override
-  Future<List<Cliente>> listar(String filtroPesquisa) {
-    // TODO: implement listar
-    throw UnimplementedError();
+  Future<List<Cliente>> listar(String filtroPesquisa) async {
+    var params = {"filtroPesquisa": filtroPesquisa};
+
+    var response =
+        await requestGET('/ws/curso-flutter/autenticar', params: params);
+
+    return Cliente.fromJson(response.body);
   }
 }
 
 class ClienteRepoInMemory implements IClienteRepo {
   @override
-  Future<List<Cliente>> listar(String filtroPesquisa) {
-    // TODO: implement listar
-    throw UnimplementedError();
+  Future<List<Cliente>> listar(String filtroPesquisa) async {
+    String strClientes =
+        await rootBundle.loadString('assets/in_memory/cliente.json');
+
+    var lista = Cliente.fromJson(strClientes);
+
+    return lista
+        .where((element) =>
+            element.nomeRazao.isCaseInsensitiveContains(filtroPesquisa) ||
+            element.nomeFantasia.isCaseInsensitiveContains(filtroPesquisa))
+        .toList();
   }
 }
